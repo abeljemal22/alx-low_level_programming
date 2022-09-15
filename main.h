594ff48cd2c7d2e90f680429b5d1d7bb225cec36
abeljemal22 <abel.j3938@gmail.com>
2022-09-15 05:42:59 -0700

    0-isupper.c

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for protoypes
+ * void protoypes(void)
+ * int protoypes(ini)
+ */
+void print_numbers(void);
+void print_most numbers(void);
+void more_numbers(void);
+void print_line(ini n);
+void print_diagonal(ini n);
+void print_square(ini size);
+void print_triangle(ini size);
+void print_numbers(ini n)
+ini _putchar(char);
+ini _isuper(ini);
+ini-isdigit(ini);
+ini_nul(ini, ini);
+
+#endif /* MAIN_H */
